878755cbb04209588945cf1e207690376eb1daa2
yenxii <dri1356@gmail.com>
2024-06-19 09:28:16 +0900

    <2024.06.19>  - 최초 커밋

---
+++ java_00_240618/src/java_00_240618/Hellojava.java
@@ -0,0 +1,19 @@
+package java_00_240618;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String test1 = "안녕";
+		
+		System.out.println(test1);
+		
+		System.out.println("Hello Java 2 !!!");
+		
+		
+		
+		
+	}
+
+}
