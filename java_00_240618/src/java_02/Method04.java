198d3e623b53443020cc037d2569aca78fb2d0d2
yenxii <dri1356@gmail.com>
2024-06-25 13:26:26 +0900

    < 2024. 06. 25 - 수업>

---
+++ java_00_240618/src/java_02/Method04.java
@@ -0,0 +1,157 @@
+package java_02;
+
+import java.util.Scanner;
+
+public class Method04 {
+
+	public static void main(String[] args) {
+
+		/*
+		[ 요구사항 ]
+		- 숫자를 2개 입력받아 연산식을 반환하는 메소드를 선언하세요.
+		- 연산식 ( + , - , * , / , % )를 각각의 메소드로 선언하세요.
+		
+		[조건 추가]
+		0
+		--------------------------
+		1. 입력 > scanner 생성
+		2. 사용할 변수 선언 및 초기화
+			: int n1, int n2, String result
+		3. 메소드 선언
+		4. 매개변수 이용 메소드 작성
+		5. 값 반환
+		6. 반환값 출력
+		 */
+
+		Scanner sc = new Scanner(System.in);
+
+		int n1 = 0;
+		int n2 = 0;
+		String result = "";
+
+		System.out.print("첫번째 숫자  : ");
+		n1 = sc.nextInt();
+
+		System.out.print("두번째 숫자 : ");
+		n2 = sc.nextInt();
+
+		// 각각의 연산식 메소드 호출
+
+		if(n1 >= 0 && n2 >= 0) {
+			result = add(n1, n2);
+			System.out.println(result);
+			
+			result = subtract(n1,n2);
+			System.out.println(result);
+			
+			result = multiply(n1, n2);
+			System.out.println(result);
+			
+			result = divide(n1, n2);
+			System.out.println(result);
+			
+			result = mod(n1, n2);
+			System.out.println(result);
+		} else {
+			System.out.println("[ERR] 0을 포함한 양수만 입력이 가능합니다.");
+		}
+		
+		
+		// 1. 메소드 호출
+//		result = add(n1, n2);
+//		System.out.println(result);
+//		
+//		result = subtract(n1,n2);
+//		System.out.println(result);
+//		
+//		result = multiply(n1, n2);
+//		System.out.println(result);
+//		
+//		result = divide(n1, n2);
+//		System.out.println(result);
+//		
+//		result = mod(n1, n2);
+//		System.out.println(result);
+		
+		
+//		// 2. 조건 추가 - 숫자는 0을 포함한 양수만 허용
+//		System.out.println("=====================================================");
+//		System.out.println("조건 추가 - 숫자는 0을 포함한 양수만 허용");
+
+		sc.close();
+
+	}
+
+	// 더하기 ( + )
+	public static String add(int n1, int n2) {
+
+		int result = 0;
+		String output = "";
+
+		result = n1 + n2;
+		output = n1 + " + " + n2 + " = " + result;
+
+		return output;
+
+	}
+
+	// 빼기 ( - )
+	public static String subtract(int n1, int n2) {
+
+		int result = 0;
+		String output = "";
+
+		result = n1 - n2;
+		output = n1 + " - " + n2 + " = " + result;
+
+		return output;
+	}
+
+	// 곱하기 ( * )
+	public static String multiply(int n1, int n2) {
+
+		int result = 0;
+		String output = "";
+
+		result = n1 * n2;
+		output = n1 + " * " + n2 + " = " + result;
+
+		return output;
+
+	}
+
+	// 나누기 몫 ( / )
+	public static String divide(int n1, int n2) {
+
+		double result = 0;
+		String output = "";
+		
+		if(n2 == 0) {
+			output = "0으로는 나눌 수 없음!!";
+			
+		} else {
+			result = (double) n1 / n2;
+			output = n1 + " / " + n2 + " = " + result;
+		}
+
+		return output;
+	}
+
+	// 나누기 나머지 ( % )
+	public static String mod(int n1, int n2) {
+
+		int result = 0;
+		String output = "";
+
+		if(n2 == 0) {
+			output = "0으로는 나눌 수 없음!!";
+			
+		} else {
+			result =  n1 % n2;
+			output = n1 + " / " + n2 + " = " + result;
+		}
+
+		return output;
+	}
+
+}
---
+++ java_00_240618/src/java_02/Method05.java
@@ -0,0 +1,46 @@
+package java_02;
+
+import java.util.Scanner;
+
+public class Method05 {
+
+	public static void main(String[] args) {
+
+		/*
+		[ 요구사항 ]
+		- 숫자를 입력받아 1부터 입력받은 숫자까지의 합을 구하시오.
+		- Scanner를 통해 숫자 입력 받기
+		- 숫자의 합을 구하는 구문은 메소드로 작성하기
+		--------------------------
+		 */
+		Scanner sc = new Scanner(System.in);
+		
+		int num = 0;
+		int sum = 0;
+		
+		System.out.println("숫자를 입력하세요 : ");
+		num = sc.nextInt();
+		
+		if (num >= 1) {
+			sum = sum(num);
+			System.out.println("1부터 " + num + "까지의 합은 "+ sum + "입니다.");
+		} else {
+			System.out.println("1보다 작은 숫자는 계산 라 수 없어요");
+		}
+		
+
+	}
+	
+	//계산식 method
+	public static int sum(int num) {
+		int result = 0;
+		
+		for (int i=1; i<=num; i++) {
+			result = result + i;
+			//result += i;
+		}
+		
+		return result;
+	}
+
+}
