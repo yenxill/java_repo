47c14079eabc1cd9e96ea265c9b1f653352f245b
yenxii <dri1356@gmail.com>
2024-06-25 13:08:21 +0900

    < 2024. 06. 19 - 강의>

--- java_00_240618/src/java_01/Hellojava.java
+++ java_00_240618/src/java_01/Hellojava.java
@@ -1,17 +1,24 @@
-package java_00_240618;
+package java_01;
+
+/**
+ * 작성자 : 권민정
+ * 작성일자 : 2024.06.18
+ * 코멘트 : 자바 예제 소스 코드
+ */
 
 public class Hellojava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//문자열 예제
 		String test1 = "안녕";
 		
 		System.out.println(test1);
 		
 		System.out.println("Hello Java 2 !!!");
 		
-		
+		String member_info = "";
 		
 		
 	}
