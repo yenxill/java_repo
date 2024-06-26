461475ae3c66cd3959d1b8f06ff34ef5728aa751
yenxii <dri1356@gmail.com>
2024-06-26 12:29:45 +0900

    < 2024. 06. 26 - 수업>

---
+++ java_00_240618/src/java_03/book/Book.java
@@ -0,0 +1,81 @@
+package java_03.book;
+
+public class Book {
+	
+	// 도서명
+	// 가격
+	// 글쓴이
+	// 출판사
+	// 발행년도
+	// ISBN
+	// 총 페이지
+
+	private String title;
+	private int price;
+	private String author;
+	private String publisher;
+	private String pubYear;
+	private String isbn;
+	private int page;
+	
+	public String bookInfo() {
+		String result = "";
+		
+		result = "=== [ 도서정보 ] ===" + "\n";
+		result += "도서명 : " + this.title + "\n";
+		result += "가격 : " + this.price + "\n";
+		result += "글쓴이 : " + this.author + "\n";
+		result += "출판사 : " + this.publisher + "\n";
+		result += "발행년도 : " + this.pubYear + "\n";
+		result += "ISBN : " + this.isbn + "\n";
+		result += "총 페이지 : " + this.page;
+		
+		
+		return result;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getPubYear() {
+		return pubYear;
+	}
+	public void setPubYear(String pubYear) {
+		this.pubYear = pubYear;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	
+
+}
---
+++ java_00_240618/src/java_03/book/Bookinfo.java
@@ -0,0 +1,25 @@
+package java_03.book;
+
+public class Bookinfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+//		Book b1 = new Book();
+//
+//		b1.setTitle("자바의 정석");
+//		b1.setPrice(45000);
+//		b1.setAuthor("남궁성");
+//		b1.setPublisher("도우출판");
+//		b1.setPubYear("2019");
+//		b1.setIsbn("8994492038");
+//		b1.setPage(1022);
+//		
+//		System.out.println(b1.bookInfo());
+		
+		BookCreate.bookInfoInsert();
+
+	}
+}
