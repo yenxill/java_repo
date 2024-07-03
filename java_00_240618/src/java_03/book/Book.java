96478ae337dc17a15823f6bb89f7b00914bb7ac5
yenxii <dri1356@gmail.com>
2024-07-03 12:42:50 +0900

    < 2024. 06. 26 - 수업>

--- java_00_240618/src/java_03/book/Book.java
+++ java_00_240618/src/java_03/book/Book.java
@@ -18,6 +18,13 @@ public class Book {
 	private String isbn;
 	private int page;
 	
+	public Book() {}
+	
+	public Book(String arg1, String arg2) {
+		this.title = arg1;
+		this.author = arg2;
+	}
+	
 	public String bookInfo() {
 		String result = "";
 		
