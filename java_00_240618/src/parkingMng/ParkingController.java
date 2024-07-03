96e3dc904a80965835b8348fa01bf1dfc5970e23
yenxii <dri1356@gmail.com>
2024-07-03 12:37:22 +0900

    < 2024. 07. 03 - 수업 프로젝트>

---
+++ java_00_240618/src/parkingMng/ParkingController.java
@@ -0,0 +1,13 @@
+package parkingMng;
+
+public class ParkingController {
+
+	public static void main(String[] args) {
+		
+		ParkingService ParkingService = new ParkingService();
+		
+		ParkingService.startProgram();
+
+	}
+
+}
