58f2b39049859cccc1615646a8ca691d94e39ec3
yenxii <dri1356@gmail.com>
2024-07-04 12:27:10 +0900

    < 2024. 07. 04 - 프로젝트 실습 수업 >

---
+++ java_00_240618/src/hotel/HotelController.java
@@ -0,0 +1,14 @@
+package hotel;
+
+public class HotelController {
+
+	public static void main(String[] args) {
+		
+		
+		HotelService hotelService = new HotelService();
+		
+		hotelService.startProgram();
+
+	}
+
+}
---
+++ java_00_240618/src/hotel/Room.java
@@ -0,0 +1,38 @@
+package hotel;
+
+public class Room {
+
+	public boolean isUsing;
+	//true : 사용중
+	//false : 사용가능(빈 객실)
+	
+	public Room(){
+	
+	}
+	
+	public Room(boolean isUsing) {
+		this.isUsing = isUsing;
+	}
+	
+
+	public boolean isUsing() {
+		return isUsing;
+	}
+	
+
+	public void setUsing(boolean isUsing) {
+		this.isUsing = isUsing;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
